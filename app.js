692fe68e2787eb9d5a93f1da58829ea90a78c75d
Yaniv Aflalo <yanivaflalo@gmail.com>
2022-02-02 23:58:28 +0200

    required dotenv on app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 
 const corsOptions = {
   credentials: true,
   origin: process.env.ORIGIN || 'http://localhost:3000',
 };
+console.log(process.env.ORIGIN, corsOptions.origin);
 
 app.use(cors(corsOptions));
 app.use(express.json());
